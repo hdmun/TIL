0a949640771dc3140171509a9c4b6061228cbff5
hdmun <huidae.mun@gmail.com>
2022-05-12 11:22:24 +0900

    슬랙 봇 메세지 리플 달기 테스트 코드 추가

--- Slack.API.DotNet/Slack.API.Net48.Tests/SlackClientTest.cs
+++ Slack.API.DotNet/Slack.API.Net48.Tests/SlackClientTest.cs
@@ -5,12 +5,12 @@ namespace Slack.API.Net48.Tests
 {
     public class SlackClientTest
     {
+        private readonly string token = "";
+        private readonly string channelId = "";
+
         [Fact]
         public void PostMessageTest()
         {
-            string token = "";
-            string channelId = "";
-
             Assert.True(token.Length > 0);
             Assert.True(channelId.Length > 0);
 
@@ -29,6 +29,34 @@ namespace Slack.API.Net48.Tests
                                     .GetResult();
 
             Assert.True(response.Ok);
+        }
+
+        [Fact]
+        public void ReplyAfterPostMessageTest()
+        {
+            Assert.True(token.Length > 0);
+            Assert.True(channelId.Length > 0);
+
+            var attachments = new Attachment[] {
+                Attachment.Create("good", "테스트 메세지", null)
+            };
+            var message = ChatPostMessage.Create(channelId, attachments, null);
+            var slackClient = new SlackClient(token);
+            var response = slackClient.PostMessage(message)
+                                    .GetAwaiter()
+                                    .GetResult();
+
+            Assert.True(response.Ok);
+
+            var replyattachments = new Attachment[] {
+                Attachment.Create("good", "리플 메세지 테스트", null)
+            };
+            var replymessage = ChatPostMessage.Create(channelId, replyattachments, response.Ts);
+            var replyresponse = slackClient.PostMessage(replymessage)
+                                    .GetAwaiter()
+                                    .GetResult();
+
+            Assert.True(replyresponse.Ok);
         }
     }
 }
