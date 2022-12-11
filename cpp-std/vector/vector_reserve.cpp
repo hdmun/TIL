8de2cf59f83b9ff6515dd01ab2ead7c00662e1cb
hdmun <huidae.mun@gmail.com>
2022-12-11 18:56:00 +0900

    stl vector, reserve / resize 메모리 변화량 체크

--- cpp-std/vector/vector_reserve.cpp
+++ cpp-std/vector/vector_reserve.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector>
 
-void vector_reserve_test()
+void basic_test()
 {
     std::cout << std::endl << "vector_reserve_test" << std::endl;
 
@@ -18,4 +18,23 @@ void vector_reserve_test()
         std::cout << static_cast<int>(vec[i]) << ", ";
     }
     std::cout << std::endl;
+}
+
+void resize_after_reserve()
+{
+    std::cout << std::endl << "resize_after_reserve" << std::endl;
+
+    std::vector<int> vec;
+    vec.reserve(10);
+    std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << ", address: " << vec.data() << std::endl;
+
+    vec.resize(5);
+    std::cout << "size: " << vec.size() << ", capacity: " << vec.capacity() << ", address: " << vec.data() << std::endl;
+}
+
+
+void vector_reserve_test()
+{
+    basic_test();
+    resize_after_reserve();
 }
