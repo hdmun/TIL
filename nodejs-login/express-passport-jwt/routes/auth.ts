df2b438ed7a60842bb9d31b6c20ec0355ce49f55
hdmun <huidae.mun@gmail.com>
2022-12-21 19:14:49 +0900

    세션 관련 패키지, 코드 제거

--- nodejs-login/express-passport-jwt/routes/auth.ts
+++ nodejs-login/express-passport-jwt/routes/auth.ts
@@ -1,10 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
-
-declare module 'express-session' {
-  interface SessionData {
-    authenticate?: boolean;
-  }
-}
+import express, { Request, Response } from 'express';
 
 interface LoginRequest {
   id: string;
@@ -13,29 +7,8 @@ interface LoginRequest {
 
 const router = express.Router();
 
-export function login(req: Request, res: Response, next: NextFunction) {
-  if (req.session.authenticate) {
-    // already login
-    res.status(201).render('index', { title: 'Already Login' })
-  } else {
-    // verify
-    // 서비스 레이어를 두는게 좋다.
-
-    const dto: LoginRequest = req.body;
-    if (dto.id === 'testid' && dto.password === 'testpassword') {
-      req.session.authenticate = true;
-      res.status(201).render('index', { title: 'Success Login' })
-    } else {
-      res.status(401)
-        .render('error', {
-          message: 'invalid id/password',
-          error: {
-            status: 401,
-            stack: []
-          }
-        })
-    }
-  }
+export function login(req: Request, res: Response) {
+  res.status(201).render('index', { title: 'Login' })
 }
 
 router.post('/login', login);
--- nodejs-login/express-passport-jwt/routes/index.ts
+++ nodejs-login/express-passport-jwt/routes/index.ts
@@ -4,11 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  if (req.session.authenticate) {
-    res.render('index', { title: 'Express' });
-  } else {
-    res.render('login')
-  }
+  res.render('index', { title: 'Express' });
 });
 
 export default router
