345e8672172417aab69933d1c57b02f78e22c961
hdmun <huidae.mun@gmail.com>
2022-12-29 22:02:53 +0900

    테스트를 위해 index 페이지 버튼 추가
    - `/signin`, `/signup`
    - `/users` 호출

--- nodejs-login/passport-jwt-react-spa/src/store.ts
+++ nodejs-login/passport-jwt-react-spa/src/store.ts
@@ -3,16 +3,21 @@
 
 import { configureStore } from "@reduxjs/toolkit"
 import { authApi } from "./service/authAPI";
+import { userApi } from "./service/usersAPI";
 import authReducer from "./slice/auth"
+import userReducer from "./slice/users"
 
 export const store = configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
     auth: authReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       authApi.middleware,
+      userApi.middleware,
     ]),
 })
 
