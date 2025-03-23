fd2693daef340f5bcead80469fc2436d6dfa54eb
hdmun <48820928+hdmun@users.noreply.github.com>
2025-03-23 10:38:12 +0900

    C# grpc 서버 예제 프로젝트 추가

---
+++ csharp-grpc-server-example/csharp-grpc-server/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+using csharp_grpc_server;
+using Grpc.Core;
+
+namespace csharp_grpc_server.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
